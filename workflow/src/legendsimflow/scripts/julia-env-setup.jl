00cc5702df3eddaebc810e2f9cdf9621afec5699
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-11-13 14:42:08 +0100

    improve julia setup

---
+++ workflow/src/legendsimflow/scripts/julia-env-setup.jl
@@ -0,0 +1,9 @@
+import Pkg
+
+if !ispath(joinpath(first(DEPOT_PATH), "registries", "LegendJuliaRegistry"))
+    @info("Installing Legend Julia package registry")
+    Pkg.Registry.add("General")
+    Pkg.Registry.add(url = "https://github.com/legend-exp/LegendJuliaRegistry.git")
+end
+
+Pkg.instantiate()
